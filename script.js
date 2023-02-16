7a8db308214db84ff8fdca02bbd69d962961adeb
toncodes <everton.vgs_liverock@hotmail.com>
2023-02-16 20:23:32 -0300

    inicio do script

---
+++ script.js
@@ -0,0 +1,58 @@
+async function readPosts(){
+  let postArea = document.querySelector('.posts')
+  postArea.innerHTML = 'Carregando...'
+
+  let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  let json = await response.json()
+
+  if(json.length > 0){
+    postArea.innerHTML = ''
+
+    for (let i in json){
+      let postHtml = `<div><h2>${json[i].title}</h2>${json[i].body}<hr></div>`
+      postArea.innerHTML += postHtml
+    }
+  } else {
+    postArea.innerHTML = "Nenhum post para exibir"
+  }
+}
+
+
+document.querySelector('button').addEventListener('click', () => {
+
+  let form = document.querySelector('div')
+  form.classList.remove('desativo')
+  form.classList.add('ativo')
+})
+
+async function addNewPost(title, body) {
+  await fetch('', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title,
+      body,
+      userId:2
+    })
+  })
+
+  document.querySelector('#titleField').value = ''
+  document.querySelector('#bodyField').value = ''
+
+  readPosts()
+}
+
+document.querySelector('#insertButton').addEventListener('click', () => {
+let title = document.querySelector('#titleField').value
+let body = document.querySelector('#bodyField').value
+
+if(title && body) {
+  addNewPost(title, body)
+} else {
+  alert('Preencha todos os campos')
+}
+})
+
+readPosts()
